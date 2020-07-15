ac11b0d46d72d18c6cc294458385127aa7aba098
Theo SIGNORE <tsignore@e2r2p22.42.fr>
2020-07-15 08:25:14 +0200

    ex03 corrected ?

--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -6,13 +6,14 @@
 /*   By: tsignore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 14:21:51 by tsignore          #+#    #+#             */
-/*   Updated: 2020/07/14 14:36:29 by tsignore         ###   ########.fr       */
+/*   Updated: 2020/07/15 08:24:53 by tsignore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_power(int nb, int power)
 {
 	nb = power > 1 ? nb * ft_recursive_power(nb, power - 1) : nb;
-	nb = power <= 0 ? 1 : nb;
+	nb = power < 0 ? 0 : nb;
+	nb = power == 0 ? 1 : nb;
 	return (nb);
 }
