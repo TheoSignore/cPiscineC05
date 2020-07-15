5a0c4935db61b2c19b250406d02cf8e5e1da636d
Theo SIGNORE <tsignore@e2r2p22.42.fr>
2020-07-15 11:27:47 +0200

    ex01 tested and corrected

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -6,14 +6,12 @@
 /*   By: tsignore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/14 12:46:03 by tsignore          #+#    #+#             */
-/*   Updated: 2020/07/14 21:39:34 by tsignore         ###   ########.fr       */
+/*   Updated: 2020/07/15 11:27:01 by tsignore         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
-	int sign;
-
 	nb = nb == 0 ? 1 : nb;
 	nb = nb < 0 ? 0 : nb;
 	if (nb - 1 > 0)
